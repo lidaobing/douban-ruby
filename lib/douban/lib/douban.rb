367546c8dcbeb4fa833f625c9f0631e9c2895a1a
hoooopo <hoooopo@4c76cd94-ba5a-11dd-8f2b-43a3dbd57a2c>
2008-11-26 23:17:40 +0000

    git-svn-id: http://doubanclient-ruby.googlecode.com/svn/trunk@43 4c76cd94-ba5a-11dd-8f2b-43a3dbd57a2c

---
+++ lib/douban/lib/douban.rb
@@ -0,0 +1,40 @@
+#=douban.rb
+#
+# Copyright (c) 2008 Hooopo
+# Written and maintained by Hooopo<wxz125627771@gmail.com>
+#
+# 
+# This program is free software. You can re-distribute and/or
+# modify this program under the same terms of ruby itself ---
+# Ruby Distribution License or GNU General Public License.
+#
+# See Douban::Authorize for an overview and examples. 
+# 
+base = File.expand_path(File.dirname(__FILE__))
+$:.unshift base + "/douban"
+$:.unshift base + "/douban/classes"
+$:.unshift base + "/douban/helper"
+require'authorize.rb'
+require'people.rb'
+require'helper'
+module Douban
+  API_CONF     =File.dirname(__FILE__)+"/../conf/douban.yaml"
+  CONF = YAML.load(File.open(API_CONF)) 
+  MY_SITE                     =CONF['mysite']
+  API_HOST                   = "http://api.douban.com"
+  OAUTH_HOST              ="http://www.douban.com"
+  REQUEST_TOKEN_PATH ="/service/auth/request_token"
+  ACCESS_TOKEN_PATH   ="/service/auth/access_token"
+  AUTHORIZE_PATH         ="/service/auth/authorize"
+ 
+  def self.authorize
+    @client = Authorize.new
+    yield @client if block_given?
+    @client
+  end
+  def self.authorization
+    @client ||= self.authorize
+  end
+end
+fail "This is a library, not a command line app" if $0 == __FILE__
+
