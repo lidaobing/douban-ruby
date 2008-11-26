55d4eefba67de3d62832e133acb9fe8ecadcd547
hoooopo <hoooopo@4c76cd94-ba5a-11dd-8f2b-43a3dbd57a2c>
2008-11-26 23:27:23 +0000

    git-svn-id: http://doubanclient-ruby.googlecode.com/svn/trunk@45 4c76cd94-ba5a-11dd-8f2b-43a3dbd57a2c

---
+++ lib/douban/classes/people.rb
@@ -0,0 +1,66 @@
+require'rexml/document'
+module Douban
+=begin
+class People
+  attr_accessor :id
+  attr_accessor:location
+  attr_accessor:title
+  attr_accessor:link
+  attr_accessor :content
+  attr_accessor :uid
+  def initialize(entry=nil)
+    if entry
+      @link={}
+    entry.each do |e|
+        if e.name=="link"
+        @link[e.attributes['rel']]=e.attributes['href']
+        else
+          instance_variable_set("@#{e.name}",e.text) 
+        end
+       self
+        end
+      end
+    end
+=end
+class People
+    include Douban
+    
+    class << self
+      def attr_names
+        [ 
+          :id,
+          :location,
+          :title,
+          :link,
+          :content,
+          :uid
+        ]
+      end
+    end
+    attr_reader :friends
+    attr_names.each do |attr|
+      attr_accessor attr
+    end
+  
+    def initialize(doc="")
+      begin
+    doc= REXML::Document.new(doc)
+       %w[id title content db:location db:uid].each do |attr|
+        eval <<-RUBY
+          REXML::XPath.each(doc, "//entry/#{attr}") do |e|
+            @#{attr.split(':').pop} = e.text if e.text
+          end
+        RUBY
+    end
+      REXML::XPath.each(doc, "//entry/link[@*]") do |e|
+        @link ||= {}
+        @link["#{e.attributes['rel']}"] = e.attributes['href']
+      end
+      rescue
+      ensure
+       self
+      end
+     
+    end
+end
+end
