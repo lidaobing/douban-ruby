a90f54a07cc3d2a6a56577a296829c8a23101dec
hoooopo <hoooopo@4c76cd94-ba5a-11dd-8f2b-43a3dbd57a2c>
2008-11-28 20:48:56 +0000

    git-svn-id: http://doubanclient-ruby.googlecode.com/svn/trunk@48 4c76cd94-ba5a-11dd-8f2b-43a3dbd57a2c

--- lib/douban/authorize.rb
+++ lib/douban/authorize.rb
@@ -2,25 +2,25 @@ require'oauth/consumer'
 require'rexml/document'
 module Douban
 class Authorize
-  include Douban
-  attr_reader :api_key
-  attr_reader :api_secret
-  attr_reader :authorize_url
-  attr_reader :consumer
-  attr_reader :request_token
-  attr_reader :access_token
-  def initialize(attributes={})
-    @api_key=attributes[:api_key] ||=CONF["api_key"]
-    @secret_key=attributes[:secret_key]||=CONF["secret_key"]
-    @oauth_option={
-     :site=>OAUTH_HOST,
-     :request_token_path=>REQUEST_TOKEN_PATH,
-     :access_token_path=>ACCESS_TOKEN_PATH,
-     :authorize_path=>AUTHORIZE_PATH 
-    }
-    yield self if block_given?
-    self 
-  end
+    include Douban
+    attr_reader :api_key
+    attr_reader :api_secret
+    attr_reader :authorize_url
+    attr_reader :consumer
+    attr_reader :request_token
+    attr_reader :access_token
+    def initialize(attributes={})
+      @api_key=attributes[:api_key] ||=CONF["api_key"]
+      @secret_key=attributes[:secret_key]||=CONF["secret_key"]
+      @oauth_option={
+        :site=>OAUTH_HOST,
+        :request_token_path=>REQUEST_TOKEN_PATH,
+        :access_token_path=>ACCESS_TOKEN_PATH,
+        :authorize_path=>AUTHORIZE_PATH 
+      }
+      yield self if block_given?
+      self 
+    end
   
   def authorized?
     !@access_token.nil?
@@ -48,44 +48,44 @@ class Authorize
     end
   end
   
-def get(path,headers={})
-  @access_token.get(path,headers)
-end
-
-def post(path,data="",headers={})
-  @access_token.post(path,data,headers)
-end
-
-def put(path,body="",headers={})
-  @access_token.put(path,body,headers)
-end
-
-def delete(path,headers={})
-  @access_token.delete(path,headers)
-end
-
-def head(path,headers={})
-  @access_token.head(path,headers)
-end
-
-def get_people(uid="@me")
-  resp=get("/people/#{url_encode(uid)}")
-  if resp.code=="200"
-    atom=resp.body
-    People.new(atom)
-    else
-    nil
+  def get(path,headers={})
+   @access_token.get(path,headers)
   end
-end
 
-def get_friends(uid="@me",option={'start-index'=>1,'max-results'=>10})
-  resp=get("/people/#{url_encode(uid)}/friends?start-index=#{option['start-index']}&max-results=#{option['max-results']}")
-if resp.code=="200"
-friends=[]
-atom=resp.body
-doc=REXML::Document.new(atom)
-REXML::XPath.each(doc,"//entry") do |entry|
-   friend = People.new    
+  def post(path,data="",headers={})
+   @access_token.post(path,data,headers)
+ end
+
+  def put(path,body="",headers={})
+   @access_token.put(path,body,headers)
+  end
+
+  def delete(path,headers={})
+    @access_token.delete(path,headers)
+  end
+
+  def head(path,headers={})
+    @access_token.head(path,headers)
+  end
+
+  def get_people(uid="@me")
+    resp=get("/people/#{url_encode(uid)}")
+    if resp.code=="200"
+      atom=resp.body
+      People.new(atom)
+    else
+      nil
+    end
+  end
+
+  def get_friends(uid="@me",option={'start-index'=>1,'max-results'=>10})
+    resp=get("/people/#{url_encode(uid)}/friends?start-index=#{option['start-index']}&max-results=#{option['max-results']}")
+    if resp.code=="200"
+      friends=[]
+      atom=resp.body
+      doc=REXML::Document.new(atom)
+      REXML::XPath.each(doc,"//entry") do |entry|
+      friend = People.new    
         %w[id title content db:location db:uid].each do |attr|
           eval <<-RUBY
             entry.each_element("#{attr}") do |e|
@@ -98,20 +98,20 @@ REXML::XPath.each(doc,"//entry") do |entry|
           friend.link["#{e.attributes['rel']}"] = e.attributes['href']
         end
         friends << friend
+      end
+    friends
+    else
+      nil
+    end
   end
-  friends
-else
- nil
-end
-end
-def get_contacts(uid="@me",option={'start-index'=>1,'max-results'=>10})
+  def get_contacts(uid="@me",option={'start-index'=>1,'max-results'=>10})
     resp=get("/people/#{url_encode(uid)}/contacts?start-index=#{option['start-index']}&max-results=#{option['max-results']}")
-if resp.code=="200"
-  contacts=[]
-atom=resp.body
-doc=REXML::Document.new(atom)
-REXML::XPath.each(doc,"//entry") do |entry|
-   contact = People.new    
+    if resp.code=="200"
+      contacts=[]
+      atom=resp.body
+      doc=REXML::Document.new(atom)
+      REXML::XPath.each(doc,"//entry") do |entry|
+      contact = People.new    
         %w[id title content db:location db:uid].each do |attr|
           eval <<-RUBY
             entry.each_element("#{attr}") do |e|
@@ -124,22 +124,22 @@ REXML::XPath.each(doc,"//entry") do |entry|
           contact.link["#{e.attributes['rel']}"] = e.attributes['href']
         end
         contacts << contact
+      end
+      contacts
+    else
+      nil
+    end
   end
-  contacts
-else
- nil
-end
-end
 
 
-def get_contacts(uid="@me",option={'start-index'=>1,'max-results'=>10})
-resp=get("/people/#{url_encode(uid)}/contacts?start-index=#{option['start-index']}&max-results=#{option['max-results']}")
-if resp.code=="200"
-contacts=[]
-atom=resp.body
-doc=REXML::Document.new(atom)
-REXML::XPath.each(doc,"//entry") do |entry|
-   contact = People.new    
+  def get_contacts(uid="@me",option={'start-index'=>1,'max-results'=>10})
+    resp=get("/people/#{url_encode(uid)}/contacts?start-index=#{option['start-index']}&max-results=#{option['max-results']}")
+    if resp.code=="200"
+      contacts=[]
+      atom=resp.body
+      doc=REXML::Document.new(atom)
+      REXML::XPath.each(doc,"//entry") do |entry|
+      contact = People.new    
         %w[id title content db:location db:uid].each do |attr|
           eval <<-RUBY
             entry.each_element("#{attr}") do |e|
@@ -152,22 +152,22 @@ REXML::XPath.each(doc,"//entry") do |entry|
           contact.link["#{e.attributes['rel']}"] = e.attributes['href']
         end
         contacts << contact
+      end
+      contacts
+    else
+      nil
+    end
   end
-contacts
-else
- nil
-end
-end
   
   
-def search_people(q="",option={'start-index'=>1,'max-results'=>10})
-resp=get("/people/?q=#{url_encode(q)}&start-index=#{option['start-index']}&max-results=#{option['max-results']}")
-if resp.code=="200"
-results=[]
-atom=resp.body
-doc=REXML::Document.new(atom)
-REXML::XPath.each(doc,"//entry") do |entry|
-   result = People.new    
+  def search_people(q="",option={'start-index'=>1,'max-results'=>10})
+    resp=get("/people/?q=#{url_encode(q)}&start-index=#{option['start-index']}&max-results=#{option['max-results']}")
+    if resp.code=="200"
+      results=[]
+      atom=resp.body
+      doc=REXML::Document.new(atom)
+      REXML::XPath.each(doc,"//entry") do |entry|
+      result = People.new    
         %w[id title content db:location db:uid].each do |attr|
           eval <<-RUBY
             entry.each_element("#{attr}") do |e|
@@ -180,12 +180,89 @@ REXML::XPath.each(doc,"//entry") do |entry|
           result.link["#{e.attributes['rel']}"] = e.attributes['href']
         end
         results << result
+      end
+      results
+    else
+      nil
+    end
   end
-  results
-else
- nil
-end
-end
-
+  def get_book(id='',isbn=false)
+    if isbn
+      resp=get("/book/subject/isbn/#{id}")
+    else
+      resp=get("/book/subject/#{id}")
+    end
+    if resp.code=="200"
+      atom=resp.body
+      Book.new(atom)
+    else
+      nil
+    end
+  end
+  def get_movie(id="",imdb=false)
+    if imdb
+      resp=get("/movie/subject/imdb/#{id}")
+    else
+      resp=get("/movie/subject/#{id}")
+    end
+    if resp.code=="200"
+      atom=resp.body
+      Movie.new(atom)
+    else
+      nil
+    end
+  end
+  def get_music(id="")
+    resp=get("/music/subject/#{id}")
+    if resp.code=="200"
+      atom=resp.body
+      Music.new(atom)
+    else
+      nil
+    end
+  end
+  def search_book(tag='',option={'start-index'=>1,'max-results'=>10})
+    resp=get("/book/subjects?tag=#{url_encode(tag)}&start-index=#{option['start-index']}&max-results=#{option['max-results']}")
+    if resp.code=="200"
+      atom=resp.body
+      doc=REXML::Document.new(atom)
+      books=[]
+      REXML::XPath.each(doc,"//entry") do |entry|
+        books << Book.new(entry.to_s)
+      end
+    else
+      nil
+    end
+    books
+  end
+  def search_movie(tag='',option={'start-index'=>1,'max-results'=>10})
+    resp=get("/movie/subjects?tag=#{url_encode(tag)}&start-index=#{option['start-index']}&max-results=#{option['max-results']}")
+    if resp.code=="200"
+      atom=resp.body
+      doc=REXML::Document.new(atom)
+      movies=[]
+      REXML::XPath.each(doc,"//entry") do |entry|
+        Movies << Movie.new(entry.to_s)
+      end
+    else
+      nil
+    end
+    movies
+  end
+  def search_music(tag='',option={'start-index'=>1,'max-results'=>10})
+    resp=get("/music/subjects?tag=#{url_encode(tag)}&start-index=#{option['start-index']}&max-results=#{option['max-results']}")
+    if resp.code=="200"
+      atom=resp.body
+      doc=REXML::Document.new(atom)
+      music=[]
+      REXML::XPath.each(doc,"//entry") do |entry|
+        music << Music.new(entry.to_s)
+      end
+    else
+      nil
+    end
+    music
+  end
+  
 end
 end
