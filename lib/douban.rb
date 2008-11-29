c0189cad771265afd650732d18b20dc91dfb3403
Hoooopo <Hoooopo@4c76cd94-ba5a-11dd-8f2b-43a3dbd57a2c>
2008-11-29 20:35:02 +0000

    git-svn-id: http://doubanclient-ruby.googlecode.com/svn/trunk@54 4c76cd94-ba5a-11dd-8f2b-43a3dbd57a2c

--- lib/douban.rb
+++ lib/douban.rb
@@ -16,9 +16,7 @@ $:.unshift base + "/douban/classes"
 $:.unshift base + "/douban/helper"
 require'authorize.rb'
 require'people.rb'
-require'books.rb'
-require'music.rb'
-require'movies.rb'
+require'subjects.rb'
 require'helper'
 module Douban
   API_CONF     =File.dirname(__FILE__)+"/../conf/douban.yaml"
