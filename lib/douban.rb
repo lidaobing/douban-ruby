8b6d75de9cc7208efcb2e276702d92d5e26f1f9a
hoooopo <hoooopo@4c76cd94-ba5a-11dd-8f2b-43a3dbd57a2c>
2008-12-07 19:04:56 +0000

    git-svn-id: http://doubanclient-ruby.googlecode.com/svn/trunk@64 4c76cd94-ba5a-11dd-8f2b-43a3dbd57a2c

--- lib/douban.rb
+++ lib/douban.rb
@@ -10,6 +10,45 @@
 #
 # See Douban::Authorize for an overview and examples. 
 # 
+#== Douban API Client Based Ruby
+# This is the Douban Ruby library, which communicates to the Douban
+# API REST servers and converts Atom into proper Ruby objects.
+# In order to use the library, you need to sign up for a Douban account and
+# ensure you have the oauth and modules installed.
+# Douban account and ApiKey,SecrectKey
+# To get a Douban account,you need to sign up at:
+# http://www.douban.com/register
+# Once approved, you will need to create an application to get your api key and
+# secret key at :
+# http://www.douban.com/service/apikey/
+# You can get apikey and secrectkey ,if you create an application.
+# The keys can be placed into douban.yaml or specified at runtime
+# when you new a Douban::Authorize.
+# Depencencies
+# This library has some external dependencies:
+# oauth     ( http://oauth.googlecode.com/svn/code/ruby )
+# You can install oauth via rubygem:
+#	Gem install oauth -y
+#==Usage
+#   require "douban"
+#   client=Douban.authorize
+#   authorize_url=client.get_authorize_url
+#  NOTE:
+#  Permission to access a user's data is restricted -- you can't just access
+#  any user's data.  In order to access a user's data, they need to have
+#  visit the authorize_url and press the 'agree' button .
+#  when user has press the 'agree' button £¬we use client.auth to authorize.
+#	 client=client.auth
+#	 puts client.authoried? #return true or false
+#  Get users' info via his uid 
+#  #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+#   people = client.get_people('hooopo')
+#   people.class === People
+#   people.title === hooopo
+#   people.location === "³¤É³"
+#==Install and Wiki
+#  http://code.google.com/p/doubanclient-ruby/
+
 base = File.expand_path(File.dirname(__FILE__))
 $:.unshift base + "/douban"
 $:.unshift base + "/douban/classes"
