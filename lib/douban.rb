a28072ccb52ba9484fbcc8357ff1638ed275899f
LI Daobing <lidaobing@gmail.com>
2010-07-02 20:45:25 +0800

    release 0.0.3

--- lib/douban.rb
+++ lib/douban.rb
@@ -55,7 +55,7 @@ end
 require 'douban/helper/helper'
 
 module Douban
-  VERSION      = "0.0.3.dev"
+  VERSION      = "0.0.3"
   API_HOST= "http://api.douban.com"
   OAUTH_HOST="http://www.douban.com"
   REQUEST_TOKEN_PATH ="/service/auth/request_token"
